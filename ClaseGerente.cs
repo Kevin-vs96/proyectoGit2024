6896702d8f63672d447278937e4e3ec6a2402aa8
Quevin Velasquez <velasquezkevin232@gmail.com>
2024-01-14 11:27:31 -0600

    se ha agregado la clase Gerente

---
+++ ClaseGerente.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROYECTOGIT01_2024
+{
+    internal class ClaseGerente
+    {
+    }
+}
